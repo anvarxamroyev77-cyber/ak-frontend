9104f497a698bf56894ebb4935afa3afe12174b5
anvarxamroyev77-cyber <anvarxamroyev77@gmail.com>
2025-09-09 23:58:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Chat from "./pages/Chat";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Chat from './pages/Chat';
+import Profile from './pages/Profile';
+import Music from './pages/Music';
+import Movies from './pages/Movies';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Chat</Link> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
